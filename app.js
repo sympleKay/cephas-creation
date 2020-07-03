0466666c3d4e642881644d2f41117192729a21ad
sympleKay <kaylove4real10@gmail.com>
2020-07-03 01:29:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.port || 2000;
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use("/", router);
 
